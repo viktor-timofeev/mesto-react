63c953aaaedc78629072c13417a06c267c845379
Viktor Timofeev <uskusstbo2@yandex.ru>
2022-10-15 14:26:54 +0300

    1

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import ImagePopup from './ImagePopup';
+import PopupWithForm from './PopupWithForm';
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
@@ -29,24 +30,41 @@ onEditAvatar = {handleEditAvatarClick}
 onAddPlace = {handleAddPlaceClick}
 />
 <Footer />
- 
+<PopupWithForm name="profile"
+                title="Редактировать профиль"
+                isOpen={isEditProfilePopupOpen}>
+  {
 
-{/*  <div className="popup popup_function_edit popup_type_light">
-    <div className="popup__container popup__container_size_small">
-      <button className="popup__close-button" type="button"></button>
-      <form className="popup__form popup__form_function_edit popup__form_height_big" name="touristProfile" method="post" novalidate>
-        <h2 className="popup__title popup__title_type_large">Редактировать профиль</h2>
-        <input id="name" className="popup__input popup__input_type_title" name="name" placeholder="Имя" value=""
-          type="text" minlength="2" maxlength="40" required/>
-        <span id="name-error" className="popup__input-error name-error"></span>
-        <input id="about" className="popup__input popup__input_type_subtitle" name="about"
-          placeholder="О себе" value="" type="text" minlength="2" maxlength="200" required/>
-        <span id="about-error" className="popup__input-error about-error"></span>
-        <button className="popup__submit-button popup__submit-button_type_inactive" type="submit"
-          disabled>Сохранить</button>
-      </form>
-    </div>
-  </div>
+<>  <input id="name" className="popup__input popup__input_type_title" name="name" placeholder="Имя" value=""
+    type="text" minlength="2" maxlength="40" required/>
+    <span id="name-error" className="popup__input-error name-error"></span>
+    <input id="about" className="popup__input popup__input_type_subtitle" name="about"
+    placeholder="О себе" value="" type="text" minlength="2" maxlength="200" required/>
+    <span id="about-error" className="popup__input-error about-error"></span>
+    <button className="popup__submit-button popup__submit-button_type_inactive" type="submit"
+    disabled>Сохранить</button>
+</>
+}
+ </PopupWithForm> 
+
+ <PopupWithForm name="card"
+                title="Новое место"
+                isOpen={isAddPlacePopupOpen}>
+
+{
+<>
+<input id="title" className="popup__input popup__input_type_name" name="title" placeholder="Название"
+value="" type="text" minlength="2" maxlength="30" required/>
+<span id="title-error" className="popup__input-error title-error"></span>
+<input id="link" className="popup__input popup__input_type_link" name="link"
+placeholder="Ссылка на картинку" value="" type="url" required/>
+<span id="link-error" className="popup__input-error link-error"></span>
+</>
+}
+</PopupWithForm> 
+
+
+{/*  
 
   <div className="popup popup_function_confirm popup_type_light">
     <div className="popup__container popup__container_size_extra-small">
@@ -77,12 +95,7 @@ onAddPlace = {handleAddPlaceClick}
       <button className="popup__close-button" type="button"></button>
       <form className="popup__form popup__form_function_add popup__form_height_big" name="newCard" method="post" novalidate>
         <h2 className="popup__title popup__title_type_large">Новое место</h2>
-        <input id="title" className="popup__input popup__input_type_name" name="title" placeholder="Название"
-          value="" type="text" minlength="2" maxlength="30" required/>
-        <span id="title-error" className="popup__input-error title-error"></span>
-        <input id="link" className="popup__input popup__input_type_link" name="link"
-          placeholder="Ссылка на картинку" value="" type="url" required/>
-        <span id="link-error" className="popup__input-error link-error"></span>
+        
         <button className="popup__submit-button popup__submit-button_type_inactive" type="submit" disabled>Создать</button>
       </form>
     </div>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,17 +2,12 @@ import React from 'react';
 
 function PopupWithForm(props) {
   return (
-    <div className={`popup popup_type_${props.name}`} >
+    <div className={`popup popup_type_${props.name}  ${props.isOpen ? 'popup_opened' : ''}`} >
     <div className="popup__container popup__container_size_small">
       <button className="popup__close-button" type="button"></button>
-      <form className="popup__form popup__form_function_add popup__form_height_big" name={`${props.name}`} method="post" novalidate>
-        <h2 className="popup__title popup__title_type_large">Новое место</h2>
-        <input id="title" className="popup__input popup__input_type_name" name="title" placeholder="Название"
-          value="" type="text" minlength="2" maxlength="30" required/>
-        <span id="title-error" className="popup__input-error title-error"></span>
-        <input id="link" className="popup__input popup__input_type_link" name="link"
-          placeholder="Ссылка на картинку" value="" type="url" required/>
-        <span id="link-error" className="popup__input-error link-error"></span>
+      <form className="popup__form" name={`${props.name}`} method="post" noValidate>
+        <h2 className="popup__title popup__title_type_large">{props.title}</h2>
+        {props.children}
         <button className="popup__submit-button popup__submit-button_type_inactive" type="submit" disabled>Создать</button>
       </form>
     </div>
