9e65e63d796ad1b190466998fcdc2e2f45f59c05
Viktor Timofeev <uskusstbo2@yandex.ru>
2022-10-17 02:47:26 +0300

    1

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-function Card(props) {
+function Card(card) {
   return (
-      <div key={props.id}>
+      <div key={card.id}>
         <div className="elements__element">
           <button className="elements__trash" type="button"></button>
           <img className="elements__image"
-          style={{ backgroundImage: `url(${props.link})` }} onClick={props.onCardClick} alt="" data-type="auto"/>
+          style={{ backgroundImage: `url(${card.link})` }} onClick={props.onCardClick} alt="" data-type="auto"/>
           <div className="elements__group">
-            <h2 className="elements__title">{props.name}</h2>
+            <h2 className="elements__title">{card.name}</h2>
             <div className="elements__like-group">
             <button className="elements__like" type="button"></button>
-            <p className="elements__like-number">{props.name}</p>
+            <p className="elements__like-number">{card.likes}</p>
           </div>
           </div>
         </div>
