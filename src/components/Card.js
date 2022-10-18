9b41c47de22371413ba1ac750243543b8ab769b8
Viktor Timofeev <uskusstbo2@yandex.ru>
2022-10-18 22:42:45 +0300

    1

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Card(props) {
 
   const handleCardClick = () => {
-    props.onCardClick(props.card);
+    props.onCardClick(props);
   } 
 
   return (
@@ -11,12 +11,12 @@ function Card(props) {
         <div className="elements__element">
           <button className="elements__trash_visible" type="button"></button>
           <img className="elements__image"
-          style={{ backgroundImage: `url(${props.card.link})` }} onClick={handleCardClick} alt="" data-type="auto"/>
+          style={{ backgroundImage: `url(${props.link})` }} onClick={handleCardClick} alt="" data-type="auto"/>
           <div className="elements__group">
-            <h2 className="elements__title">{props.card.name}</h2>
+            <h2 className="elements__title">{props.name}</h2>
             <div className="elements__like-group">
             <button className="elements__like" type="button"></button>
-            <p className="elements__like-number">{props.card.likes.length}</p>
+            <p className="elements__like-number">{props.likes.length}</p>
           </div>
           </div>
         </div>
