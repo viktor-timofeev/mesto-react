216ad1479e049a8dbba0effe98d646d3a9cdffb1
Viktor Timofeev <uskusstbo2@yandex.ru>
2022-10-19 14:15:41 +0300

    1

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import api from '../utils/Api';
 import Card from './Card';
 
-function Main(props) {
+function Main({onEditProfile, onEditAvatar, onAddPlace, onCardClick}) {
   const [userName,  setUserName] = React.useState('');
   const [userDescription, setUserDescription] = React.useState('');
   const [userAvatar, setUserAvatar] = React.useState('');
@@ -32,20 +32,20 @@ function Main(props) {
       <div className="profile__item">
         <div className="profile__edit-image">
         <img className="profile__image" alt="" style={{ backgroundImage: `url(${userAvatar})` }} id="id-avatar"/>
-        <button className="profile__photo-edit-button" onClick={props.onEditAvatar} type="button"></button>
+        <button className="profile__photo-edit-button" onClick={onEditAvatar} type="button"></button>
         </div>
         <div className="profile__info">
           <div className="profile__title-block">
             <h1 className="profile__title" id="id-name">Жак-Ив Кусто</h1>
-            <button className="profile__edit-button" onClick={props.onEditProfile} type="button"></button>
+            <button className="profile__edit-button" onClick={onEditProfile} type="button"></button>
           </div>
           <p className="profile__subtitle" id="id-about">Исследователь океана</p>
         </div>
       </div>
-      <button className="profile__add-button" onClick={props.onAddPlace} type="button"></button>
+      <button className="profile__add-button" onClick={onAddPlace} type="button"></button>
     </section>
     <section className="elements">
-    {cards.map(card => <Card card={card} onCardClick={props.handleCardClick}/>
+    {cards.map(_id, ...props => <Card key={_id} props onCardClick={onCardClick}/>
     )}
     </section>
   </main>  
